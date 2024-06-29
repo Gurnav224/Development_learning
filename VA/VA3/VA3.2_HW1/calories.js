4032948ad638abe0795603b830b2fad2690911c4
Gurnav224 <chaudharyg856@gmail.com>
2024-06-29 18:52:33 +0530

    done VA3.2_HW1

---
+++ VA/VA3/VA3.2_HW1/calories.js
@@ -0,0 +1,36 @@
+const caloriesJson = [
+    {
+      "id": 1,
+      "Day": "Day1",
+      "caloriesConsumed": 234,
+      "caloriesBurned": 234
+    },
+    {
+      "id": 2,
+      "Day": "Day2",
+      "caloriesConsumed": 300,
+      "caloriesBurned": 200
+    },
+    {
+      "id": 3,
+      "Day": "Day3",
+      "caloriesConsumed": 250,
+      "caloriesBurned": 180
+    },
+    {
+      "id": 4,
+      "Day": "Day4",
+      "caloriesConsumed": 280,
+      "caloriesBurned": 220
+    },
+    {
+      "id": 5,
+      "Day": "Day5",
+      "caloriesConsumed": 260,
+      "caloriesBurned": 210
+    }
+  ]
+
+
+  export default caloriesJson;
+
