a172fc8f51a85034a9010a1e678d76ec9fffa9b1
Gurnav224 <chaudharyg856@gmail.com>
2024-07-01 18:52:07 +0530

    Done VA4 Assignment

---
+++ VA/VA4/VA4_Assignment/movie.js
@@ -0,0 +1,88 @@
+const movieJSON = [
+  {
+    "id": 1,
+    "title": "The Grand Adventure",
+    "genre": "Comedy",
+    "releaseDate": "23-12-2024",
+    "director": "John Smith"
+  },
+  {
+    "id": 2,
+    "title": "Mystery in the Mist",
+    "genre": "Drama",
+    "releaseDate": "15-01-2025",
+    "director": "John Smith"
+  },
+  {
+    "id": 3,
+    "title": "Love and Thunder",
+    "genre": "Action",
+    "releaseDate": "14-02-2024",
+    "director": "Alice Johnson"
+  },
+  {
+    "id": 4,
+    "title": "Space Odyssey",
+    "genre": "Action",
+    "releaseDate": "10-11-2024",
+    "director": "Michael Brown"
+  },
+  {
+    "id": 5,
+    "title": "The Haunted House",
+    "genre": "Horror",
+    "releaseDate": "31-10-2024",
+    "director": "Emily White"
+  },
+  {
+    "id": 6,
+    "title": "Laughter in the Park",
+    "genre": "Comedy",
+    "releaseDate": "23-05-2024",
+    "director": "John Smith"
+  },
+  {
+    "id": 7,
+    "title": "Deep Waters",
+    "genre": "Drama",
+    "releaseDate": "12-08-2025",
+    "director": "Jane Doe"
+  },
+  {
+    "id": 8,
+    "title": "Ghosts of the Past",
+    "genre": "Horror",
+    "releaseDate": "13-03-2024",
+    "director": "Michael Brown"
+  },
+  {
+    "id": 9,
+    "title": "Battlefield",
+    "genre": "Action",
+    "releaseDate": "22-06-2024",
+    "director": "Alice Johnson"
+  },
+  {
+    "id": 10,
+    "title": "Midnight Frights",
+    "genre": "Horror",
+    "releaseDate": "30-09-2024",
+    "director": "Emily White"
+  },
+  {
+    "id": 11,
+    "title": "Comic Relief",
+    "genre": "Comedy",
+    "releaseDate": "17-07-2024",
+    "director": "Jane Doe"
+  },
+  {
+    "id": 12,
+    "title": "Dark Shadows",
+    "genre": "Horror",
+    "releaseDate": "27-10-2024",
+    "director": "Emily White"
+  }
+]
+
+  export default movieJSON
