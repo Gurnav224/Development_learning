3bd84dbf37e7c9d248a17b8020c8261a1ece032a
Gurnav224 <chaudharyg856@gmail.com>
2024-07-01 12:56:07 +0530

    done VA3.2_HW2

---
+++ VA/VA3/VA3.2_HW2/movie.js
@@ -0,0 +1,41 @@
+
+const movieJSON = [
+    {
+      "id": 1,
+      "title": "The Grand Adventure",
+      "genre": "Comedy",
+      "releaseDate": "23-12-2024",
+      "director": "John Smith"
+    },
+    {
+      "id": 2,
+      "title": "Mystery in the Mist",
+      "genre": "Mystery",
+      "releaseDate": "15-01-2025",
+      "director": "Jane Doe"
+    },
+    {
+      "id": 3,
+      "title": "Love and Thunder",
+      "genre": "Romance",
+      "releaseDate": "14-02-2024",
+      "director": "Alice Johnson"
+    },
+    {
+      "id": 4,
+      "title": "Space Odyssey",
+      "genre": "Sci-Fi",
+      "releaseDate": "10-11-2024",
+      "director": "Michael Brown"
+    },
+    {
+      "id": 5,
+      "title": "The Haunted House",
+      "genre": "Horror",
+      "releaseDate": "31-10-2024",
+      "director": "Emily White"
+    }
+  ]
+
+
+  export default movieJSON
