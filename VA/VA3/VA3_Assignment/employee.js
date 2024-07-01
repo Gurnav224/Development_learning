4d45785e55ef76f857cc6e6a0a9cdb2b801e6dc4
Gurnav224 <chaudharyg856@gmail.com>
2024-07-01 12:57:05 +0530

    done VA3 Assignment

---
+++ VA/VA3/VA3_Assignment/employee.js
@@ -0,0 +1,36 @@
+
+const employeeJSON = [
+    {
+      "id": "1",
+      "name": "John Doe",
+      "title": "Software Developer",
+      "department": "IT"
+    },
+    {
+      "id": "2",
+      "name": "Jane Smith",
+      "title": "Designer",
+      "department": "Design"
+    },
+    {
+      "id": "3",
+      "name": "Bob Johnson",
+      "title": "Project Manager",
+      "department": "Management"
+    },
+    {
+      "id": "4",
+      "name": "Alice Williams",
+      "title": "Quality Assurance",
+      "department": "QA"
+    },
+    {
+      "id": "5",
+      "name": "Michael Brown",
+      "title": "Database Administrator",
+      "department": "IT"
+    }
+  ]
+
+
+  export default employeeJSON;
