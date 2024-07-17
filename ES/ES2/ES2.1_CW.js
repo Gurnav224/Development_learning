67f1ba9ae186226fa35688c79d4711ee358722b5
Gurnav224 <chaudharyg856@gmail.com>
2024-07-17 14:37:41 +0530

    Done ES2.1_CW

---
+++ ES/ES2/ES2.1_CW.js
@@ -0,0 +1,24 @@
+// traditional fuction 
+
+// function sum(a,b){
+//     return a+b
+// }
+
+// console.log(sum(10,12))
+
+// ES6- Arrow Fuction
+
+const sum =(a,b)=>a+b;
+
+console.log(sum(5,2))
+
+// multiply two number 
+
+const multiplyTwoNumber = (a,b)=>a*b;
+console.log(multiplyTwoNumber(10,2));
+
+// concatination Two String
+
+const concatinateStrings = (str1,str2)=>str1+str2;
+
+console.log(concatinateStrings('hello','world'))
