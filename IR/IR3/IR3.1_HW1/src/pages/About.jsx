47790df83601b0deb975cee98a0e6643f6db9c29
Gurnav224 <chaudharyg856@gmail.com>
2024-08-20 21:02:00 +0530

    Done IR3.1_HW1

---
+++ IR/IR3/IR3.1_HW1/src/pages/About.jsx
@@ -0,0 +1,17 @@
+
+const About = () => {
+  return (
+    <main className="container py-4">
+      <h1 className="display-3 ">About us</h1>
+      <p className="lh-3">Welcome to our restaurant guide! We are passionate about exploring and sharing culinary experiences from around the world. Our mission is connect to food lovers with the best dinning options and unforgettable flavours.</p>
+      <p className="lh-3">
+        Our team of food enthusiasts , chefs and reviewers collaborates to curate a diverse collection of restaurant recommendations, highlighting the richness of global cuisines and creativity of chefs
+      </p>
+    <p className="lh-3">
+      Join us we embark on a gastronomics journey, discovering hidden gems, iconic eateries and innovative culinary trends Whether you're a seasoned foodie or adventurous dinner, we invite you to explore , indulge, and savour the flavours of the world with us. Thank you for being part of our culinary community!
+    </p>
+    </main>
+  )
+}
+
+export default About
---
+++ IR/IR3/IR3.1_HW1/src/pages/Restaurants.jsx
@@ -0,0 +1,44 @@
+
+const Restaurants = () => {
+  return (
+    <main className="container py-4">
+    <h1 className="display-4">Restaurants</h1>
+    <div className="card mb-3">
+        <div className="row g-0">
+            <div className="col-md-4">
+                <img src="https://images.pexels.com/photos/776538/pexels-photo-776538.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="Restaurant 1" className="img-fluid  rounded-start  rounded-md-bottom" />
+            </div>
+            <div className="col-md-8">
+            <div className="card-body">
+            <h3 className="card-title">Taste of India</h3>
+            <p>Enjoy authentic Indian cuisine at taste of india. Explore the variety of delicious dishes from different regions of India</p>
+            <p className="text-muted lh-3">Rating: 4.7  | Open Now</p>
+            </div>
+            </div>
+           
+        </div>
+    </div>
+
+    <div className="card mb-3">
+        <div className="row g-0">
+            <div className="col-md-4">
+                <img src="https://images.pexels.com/photos/2898205/pexels-photo-2898205.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="Restaurant 3" className="img-fluid rounded-start "/>
+            </div>
+            <div className="col-md-8">
+                <div className="card-body">
+                    <h3 className="card-title">Pizza Paradise</h3>
+                    <p>Indulge in mouth-watering pizza at Pizza Paradise, with a variety of toppings and crusts. it's a pizza lover's dream </p>
+                    <p className="text-muted lh-3">Rating: 4.8 | Open Now</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    </main>
+  )
+}
+
+
+
+
+  export default Restaurants;
+  
