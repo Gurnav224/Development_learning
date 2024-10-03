0953181b99688f7cfcfd3b2bf7c79efa0c4e8c75
Gurnav224 <chaudharyg856@gmail.com>
2024-10-03 16:59:46 +0530

    backend completed

---
+++ major_project_part1/backend/models/category.model.js
@@ -0,0 +1,34 @@
+
+const mongoose = require('mongoose');
+const {Schema} = mongoose
+
+const CategorySchema = new Schema({
+ name:{
+    type:String,
+    required:true
+ },
+ description:{
+    type:String,
+    required:true
+ },
+ icon:{
+    type:String,
+    required:true
+ },
+ subcategories:[
+    {
+        type:String,
+    }
+ ],
+ featuredProducts:[
+    {
+        type:String
+    }
+ ]
+
+},{timestamps:true})
+
+
+const Category =  mongoose.model('Category',CategorySchema);
+
+module.exports = Category;
