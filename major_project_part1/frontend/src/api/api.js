015fd4117c289314f51392482ff6c9641e60425c
Gurnav224 <chaudharyg856@gmail.com>
2024-10-05 20:45:47 +0530

    feat: add product api

--- major_project_part1/frontend/src/api/api.js
+++ major_project_part1/frontend/src/api/api.js
@@ -3,8 +3,15 @@ const baseUrl = 'http://localhost:3000/api';
 
 const api = {
     getFeaturedCategory: async ()=> {
-       const response = await fetch(`${baseUrl}/categories`);
-       return response.json()
+            const response = await fetch(`${baseUrl}/categories`);
+            return response.json()
+       
+      
+    },
+    getAllProducts:async () => {
+            const response = await fetch(`${baseUrl}/products`);
+            return response.json()
+        
     }
 }
 
