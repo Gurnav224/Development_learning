76e8adb4374ee648932707f54b301fc6b171c9be
Gurnav224 <chaudharyg856@gmail.com>
2025-01-10 19:52:38 +0530

    js interview

--- js_interviews/JS_Interview_HW2/topics.md
+++ js_interviews/JS_Interview_HW2/topics.md
@@ -1,4 +1,4 @@
 
 ## JS_Interview _HW2 
-
+- this keyword
 - Implicit vs Explicit Bindings
---
+++ js_interviews/JS_Interview_HW3/index.js
@@ -0,0 +1,12 @@
+
+
+
+console.log('start') // 
+
+
+setTimeout(() => {
+console.log('I am spider man')
+},1000)
+
+
+console.log('I am batman') // 
